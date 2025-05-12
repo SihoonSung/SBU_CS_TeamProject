613c5717a4ec83aba3007f296aa70938af3b9ccd
mingu8908 <mingu10133@gmail.com>
2025-05-12 09:48:58 +0900

    push

--- CSE316/dugout/src/App.jsx
+++ CSE316/dugout/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Home from './home';xw
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <>
+        <nav>
+          <div className="nav-container">
+            <ul className="navbar">
+              <li className="hideOnMobile">
+                <Link to="/facility-list">Facility List</Link>
+              </li>
+              <li className="hideOnMobile">
+                <Link to="/facility-reservation">Facility Reservation</Link>
+              </li>
+              <li className="menu-button" onClick={toggleSidebar}>
+                <svg id="menu" xmlns="http://www.w3.org/2000/svg" height="25px" viewBox="0 -1200 960 960" width="25px" fill="white">
+                  <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+                </svg>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <ul className="sidebar" id="sidebar" style={{ display: sidebarVisible ? 'flex' : 'none' }}>
+          <li>
+            <Link to="/facility-list">Facility List</Link>
+          </li>
+          <li>
+            <Link to="/facility-reservation">Facility Reservation</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/facility-list" element={<FacilityList />} />
+          <Route path="/facility-reservation" element={<FacilityReservation />} />
+        </Routes>
+      </>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ CSE316/dugout/src/home.jsx
@@ -0,0 +1,38 @@
+function Home() {
+    return (
+        <div className="text" style={{ color: "black" }}>
+            <ul>
+                <li>Facility Reservation
+                    <ul>
+                        <li>
+                            Facility List
+                            <ol>
+                                <li>Reservation Date should be the date after today.</li>
+                                <li>The number of users should be between the maximum number of people and the minimum number of people.</li>
+                                <li>If the facilicy is available only for SUNY Korea, user should be in SUNY Korea.</li>
+                                <li>The reservation date must be made on the available day of the week.</li>
+                                <li>The same person cannot book another facility on the same date.</li>
+                                If all conditions are met, data is stored in local storage.
+                            </ol>
+                        </li>
+                        <li>
+                            User Information
+                            <ol>
+                                <li>user profile, user email, user password, user name</li>
+                                <li>All other details can be modified except for the user email.</li>
+                                <li>If the user profile is changed, the image in the navbar will also change.</li>
+                            </ol>
+                        </li>
+                        <li>
+                            Reservation History
+                            <p>Load the reservation data stored in the local storage.</p>
+                            <p>reservation id, facility name, purpose, peopleNum, isSUNY, booker name, date</p>
+                            <p>Can cancel reservation</p>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    );
+}
+export default Home;
