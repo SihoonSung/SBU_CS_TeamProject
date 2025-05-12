9e46ae2361581b6e3f75db825880f44c9421a011
mingu8908 <mingu10133@gmail.com>
2025-05-12 10:43:29 +0900

    commit

--- CSE316/dugout/src/home.jsx
+++ CSE316/dugout/src/home.jsx
@@ -1,6 +1,6 @@
 function Home() {
     return (
-        <div className="text" style={{ color: "black" }}>
+        <div className="text">
             <ul>
                 <li>Facility Reservation
                     <ul>
